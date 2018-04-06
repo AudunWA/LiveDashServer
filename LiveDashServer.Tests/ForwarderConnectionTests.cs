b0bb69787f065137c139e7fb263ce1d7e57948cb
Audun Wigum Arbo <audunwar@stud.ntnu.no>
2018-04-06 14:48:54 +0200

    Add more unit tests

---
+++ LiveDashServer.Tests/ForwarderConnectionTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace LiveDashServer.Tests
+{
+    class ForwarderConnectionTests
+    {
+        // The class is currenlty hard to test
+    }
+}
